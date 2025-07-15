762fa2bcd69bea921a9b911b5a7ce8cb7f9a07c1
Adarsh Kumar <125640775+adarsh-au-ki@users.noreply.github.com>
2025-07-15 17:43:56 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,12 +1,12 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    user: process.env.DATABASE_USER,
-    host: process.env.DATABASE_HOST,
-    database: process.env.DATABASE_NAME,
-    password: process.env.DATABASE_PASSWORD,
-    port: process.env.DATABASE_PORT,
-    ssl: process.env.DATABASE_SSL === 'true' ? {
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+    ssl: process.env.DB_SSL === 'true' ? {
         rejectUnauthorized: false
     } : false
 });
